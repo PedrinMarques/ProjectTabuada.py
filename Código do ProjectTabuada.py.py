3d0fb8d7b014593ef915267d82f83604d215c52f
PedrinMarques <pedrosouzamarques33@gmail.com>
2025-07-19 15:56:46 -0300

    Aplicação do código no ProjectTabuada.Py
    Na atualização "Aplicação do código no ProjectTabuada.py", eu apliquei o código do projeto, no projeto, para ele finalmente funcionar.

---
+++ Código do ProjectTabuada.py.py
@@ -0,0 +1,37 @@
+#Esse é o ProjectTabuada.py
+
+print('Olá senhor(a), esse é o ProjectTabuada.py: ')
+nome=input('Digite o seu nome: ')
+print('Ok {}, agora você pode testar o ProjectTabuada.py.'.format(nome))
+print('Faça oque pedir abaixo: ')
+
+#n=número
+
+n1=int(input('Digite um número: ')) 
+
+#t=Tabuada
+t0=n1*0
+t1=n1*1
+t2=n1*2
+t3=n1*3
+t4=n1*4
+t5=n1*5
+t6=n1*6
+t7=n1*7
+t8=n1*8
+t9=n1*9
+t10=n1*10
+
+print('{}.0 = {}'.format(n1,t0))
+print('{}.1 = {}'.format(n1,t1))
+print('{}.2 = {}'.format(n1,t2))
+print('{}.3 = {}'.format(n1,t3))
+print('{}.4 = {}'.format(n1,t4))
+print('{}.5 = {}'.format(n1,t5))
+print('{}.6 = {}'.format(n1,t6))
+print('{}.7 = {}'.format(n1,t7))
+print('{}.8 = {}'.format(n1,t8))
+print('{}.9 = {}'.format(n1,t9))
+print('{}.10 = {}'.format(n1,t10))
+
+fim=input('E aí {}, você gostou do resultado do ProjectTabuada.py? '.format(nome))
