1f5f42f5f0bd661ca6ac4e10320144ed6be0fb53
PedrinMarques <pedrosouzamarques33@gmail.com>
2025-07-20 17:27:58 -0300

    Comit 1.2.5
    Atualizei a escritura do código para uma escritura mais recente, mais detalhes no arquivo de atualização.

--- Código do ProjectTabuada.py.py
+++ Código do ProjectTabuada.py.py
@@ -2,12 +2,12 @@
 
 print('Olá senhor(a), esse é o ProjectTabuada.py, esse projeto faz tabuadas de 0 a 20, \n bem para iníciarmos com o projeto, faça oque se pede: ')
 nome=input('Digite o seu nome: ')
-print('Ok {}, agora você pode testar o ProjectTabuada.py.'.format(nome))
+print(f'Ok {nome}, agora você pode testar o ProjectTabuada.py.')
 print('Faça oque pedir abaixo: ')
 
 #n=número
 
-n1=int(input('Digite um número: ')) 
+n1=int(input(f'{nome}, digite um número: ')) 
 
 #t=Tabuada
 t0=n1*0
@@ -32,32 +32,32 @@ t18=n1*18
 t19=n1*19
 t20=n1*20
 
-print('{}.0 = {}'.format(n1,t0))
-print('{}.1 = {}'.format(n1,t1))
-print('{}.2 = {}'.format(n1,t2))
-print('{}.3 = {}'.format(n1,t3))
-print('{}.4 = {}'.format(n1,t4))
-print('{}.5 = {}'.format(n1,t5))
-print('{}.6 = {}'.format(n1,t6))
-print('{}.7 = {}'.format(n1,t7))
-print('{}.8 = {}'.format(n1,t8))
-print('{}.9 = {}'.format(n1,t9))
-print('{}.10 = {}'.format(n1,t10))
+print(f'{n1}.0 = {t0}')
+print(f'{n1}.1 = {t1}')
+print(f'{n1}.2 = {t2}')
+print(f'{n1}.3 = {t3}')
+print(f'{n1}.4 = {t4}')
+print(f'{n1}.5 = {t5}')
+print(f'{n1}.6 = {t6}')
+print(f'{n1}.7 = {t6}')
+print(f'{n1}.8 = {t8}')
+print(f'{n1}.9 = {t9}')
+print(f'{n1}.10 = {t10}')
 
 #c=Continuação
 #rp=Resposta do Progama
-c1=input('Agora {}, você gostaria de ver a tabuada desse número de 11 a 20? '.format(nome))
+c1=input(f'Agora {nome}, você gostaria de ver a tabuada desse número de 11 a 20? ')
 rp=input('Bem..... independentimente da sua resposta, o progama vai executar, \n então digite oque você acha disso:  ')
 
-print('{}.11 = {}'.format(n1,t11))
-print('{}.12 = {}'.format(n1,t12))
-print('{}.13 = {}'.format(n1,t13))
-print('{}.14 = {}'.format(n1,t14))
-print('{}.15 = {}'.format(n1,t15))
-print('{}.16 = {}'.format(n1,t16))
-print('{}.17 = {}'.format(n1,t17))
-print('{}.18 = {}'.format(n1,t18))
-print('{}.19 = {}'.format(n1,t19))
-print('{}.20 = {}'.format(n1,t20))
+print(f'{n1}.11 = {t11}')
+print(f'{n1}.12 = {t12}')
+print(f'{n1}.13 = {t13}')
+print(f'{n1}.14 = {t14}')
+print(f'{n1}.15 = {t15}')
+print(f'{n1}.16 = {t16}')
+print(f'{n1}.17 = {t17}')
+print(f'{n1}.18 = {t18}')
+print(f'{n1}.19 = {t19}')
+print(f'{n1}.20 = {t20}')
 
-fim=input('{}, oque você achou do projeto ProjectTabuada.py? '.format(nome))
+fim=input(f'{nome}, oque você achou do projeto ProjectTabuada.py? ')
